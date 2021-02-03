a8d95129e24c57433dd085981935ed3ca809a31a
SPARRok <niki.vorobiov@gmail.com>
2021-02-03 08:08:56 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DokaPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnNewAcc = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            this.txttest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(376, 262);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(176, 40);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            // 
+            // btnNewAcc
+            // 
+            this.btnNewAcc.Location = new System.Drawing.Point(340, 308);
+            this.btnNewAcc.Name = "btnNewAcc";
+            this.btnNewAcc.Size = new System.Drawing.Size(249, 40);
+            this.btnNewAcc.TabIndex = 1;
+            this.btnNewAcc.Text = "New Account";
+            this.btnNewAcc.UseVisualStyleBackColor = true;
+            this.btnNewAcc.Click += new System.EventHandler(this.BtnNewAcc_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Location = new System.Drawing.Point(894, 12);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 42);
+            this.btnSettings.TabIndex = 2;
+            this.btnSettings.Text = "*";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(164, 48);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(75, 23);
+            this.test.TabIndex = 3;
+            this.test.Text = "button1";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.Test_Click);
+            // 
+            // txttest
+            // 
+            this.txttest.Location = new System.Drawing.Point(245, 49);
+            this.txttest.Name = "txttest";
+            this.txttest.Size = new System.Drawing.Size(376, 22);
+            this.txttest.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 570);
+            this.Controls.Add(this.txttest);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnNewAcc);
+            this.Controls.Add(this.btnLogIn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DokaPass - Menu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Button btnNewAcc;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.TextBox txttest;
+    }
+}
+
